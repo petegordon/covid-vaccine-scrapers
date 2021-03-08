afab9e87863cf25340044366a02a3fef1e19cb4c
Pete Gordon <pete@petegordon.com>
2021-03-08 07:57:04 -0500

    found the vaccine information and scraped all kroger zipcodes in like 3+ hours

--- site-scrapers/Kroger/pup_scrape_vaccine_zipslots.js
+++ site-scrapers/Kroger/pup_scrape_vaccine_zipslots.js
@@ -8,7 +8,7 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 startTime = new Date()
 zipStartTime = new Date()
-storesDir = "/Users/petegordon/CantStopColumbus/covid-vaccine-scrapers/site-scrapers/Kroger/Vaccines/"
+storesDir = "/Users/petegordon/CantStopColumbus/covid-vaccine-scrapers/site-scrapers/Kroger/ZipSlots/"
 
 console.log("START:"+startTime)
 
@@ -34,12 +34,18 @@ if(process.argv[2]){
     console.log(process.cwd())
     //zipParam = JSON.parse(fs.readFileSync('ohio_zips.json'))
     //zipParam = JSON.parse(fs.readFileSync('kroger_zipcodes.json'))
-    zipParam = JSON.parse(fs.readFileSync('/Users/petegordon/CantStopColumbus/covid-vaccine-scrapers/site-scrapers/Kroger/all_stores_unique.json'))
+    zipParam = JSON.parse(fs.readFileSync('/Users/petegordon/CantStopColumbus/covid-vaccine-scrapers/site-scrapers/Kroger/kroger_zipcodes.json'))
     //zipParam = zipParam.slice(0,10)
     let storesProcessed = fs.readdirSync(storesDir)
-    zipProcessed = storesProcessed.map((f) => { return f.split('_')[2].split('.')[0]})
+    zipProcessed = storesProcessed.map((f) => { 
+        zipcode = f.split('_')[2].split('.')[0]
+        time = parseInt(f.split('_')[1])
+        if (time > (new Date().getTime() - (1000 * 60 * 60 * 3))){
+            return zipcode
+        }
+    })
 
-    zipParam = zipParam.filter((z) => !zipProcessed.includes(z.facilityId))
+    zipParam = zipParam.filter((z) => !zipProcessed.includes(z))
 
     console.log("create vaccine location length:"+zipParam.length)
 }
@@ -51,8 +57,8 @@ const myEmitter = new ScrapeEmitter();
 myEmitter.on('processZipCodes', (page) => {
     console.log('a processZipCodes event occurred!');
     if(zipParam.length > 0){
-        zipToProcess = zipParam[0].address.zipCode
-        facilityToProcess = zipParam[0].facilityId
+        zipToProcess = zipParam[0]
+        facilityToProcess = null
         //zipParam = zipParam.slice(1)
         if(manualProcessing){
             const rl = readline.createInterface({
@@ -62,9 +68,9 @@ myEmitter.on('processZipCodes', (page) => {
             rl.question(`Continue with facility: ${facilityToProcess} and ${zipToProcess}? `, (fac) => { 
                 myEmitter.emit("searchStores", fac, page)
             })
-            rl.write(facilityToProcess);
+            rl.write(zipToProcess);
         } else {
-             myEmitter.emit("searchStores", facilityToProcess, page)          
+             myEmitter.emit("searchStores", zipToProcess, page)          
         }
 
         
@@ -73,13 +79,13 @@ myEmitter.on('processZipCodes', (page) => {
         console.log("END:"+new Date())        
     }
 })
-myEmitter.on('searchStores', async (fac, page) => {
+myEmitter.on('searchStores', async (zip, page) => {
     zipStartTime = new Date()
-   let facilityObj = zipParam.filter((s) => {return s.facilityId == fac })
+  // let facilityObj = zipParam.filter((s) => {return s.facilityId == fac })
    zipParam = zipParam.slice(1)
 
-   console.log(fac)
-    let zip = facilityObj[0].address.zipCode
+   console.log(zip)
+//    let zip = facilityObj[0].address.zipCode
 
 
     await page.on('response', async (response) => { 
@@ -93,32 +99,37 @@ myEmitter.on('searchStores', async (fac, page) => {
             processedCount++    
             processing = false
 
-            console.log('an searchStores event occurred!'+fac);
-            await page.goto('https://www.kroger.com/rx/api/anonymous/scheduler/reasons/pharmacy/'+fac);
+            console.log('an searchStores event occurred!'+zip);
+            await page.goto("https://www.kroger.com/rx/api/anonymous/scheduler/slots/locationsearch/pharmacy/"+zip+"/2021-03-07/2021-03-14/50?appointmentReason=122&appointmentReason=125&appointmentReason=129");
+            
                     
         }
 
-        if (response.url().endsWith(fac) && response.status() == 200){
+        if (!response.url().endsWith(zip) && response.url().indexOf(zip) > 0 && response.status() == 200){
             console.log(response.url())
             console.log(response.status())      
             json = await response.json()
             await delay(6000)            
-            myEmitter.emit('foundStores', fac, json, page);                   
+            myEmitter.emit('foundStores', zip, json, page);                   
         }
 
-        if( response.url().endsWith(fac) && response.status() != 200){
+        if (!response.url().endsWith(zip) && response.url().indexOf(zip) > 0  && response.status() != 200){
             console.log(response.url())
             console.log(response.status())             
             console.log("START:"+startTime)
             console.log("END:"+new Date())      
             //process.exit()
+            /*
             const rl2 = readline.createInterface({
                 input: process.stdin,
                 output: process.stdout
             });        
             rl2.question(`Continue by starting over? `, () => { 
                 myEmitter.emit('processZipCodes', page);
-            })           
+            })  
+            */  
+           await delay(60000)
+           myEmitter.emit('processZipCodes', page);       
         }
 
     });  
@@ -135,15 +146,42 @@ myEmitter.on('searchStores', async (fac, page) => {
         await page.type('.PharmacyLocator [name="findAStore"]', c)                
     }
     processing = true
+    await delay(2000)
     page.click('.PharmacyLocator [aria-label="search"]')
 
-    console.log('get stores for zip:'+zip)     
+    console.log('get stores for zip:'+zip) 
+    await delay(4000) 
+    try{
+        
+        let securityCheck = await page.evaluate(() => {
+            let el = document.querySelector("#sec-overlay") 
+            val = "none"
+            if(el){
+                val = window.getComputedStyle(el).getPropertyValue("display")   
+            }   
+            return val != "none"
+        })
+        console.log(securityCheck)   
+        if(securityCheck){
+            await delay(300000)
+            page.goto('https://www.kroger.com/')
+            await delay(6000)
+            myEmitter.emit('processZipCodes', page);
+        }
+    }catch(ex){
+        console.log(ex)        
+        console.log('caught exception')
+
+        await delay(5000)
+        await page.goto('https://www.kroger.com/rx/guest/get-vaccinated');
+        myEmitter.emit('processZipCodes', page);
+    }
 
 
 });
 myEmitter.on('foundStores', (zip, stores, page) => {
     console.log('an foundStores event occurred! '+zip);
-    fs.writeFileSync(storesDir+'vaccines_facility_'+zip+'.json', JSON.stringify(stores, null, 2))
+    fs.writeFileSync(storesDir+'slots_'+new Date().getTime()+'_'+zip+'.json', JSON.stringify(stores, null, 2))
     console.log("ZIP PROCESS START:"+zip+":"+zipStartTime)
     console.log("ZIP PROCESS END:"+zip+":"+new Date())  
     myEmitter.emit("processZipCodes", page)
@@ -154,6 +192,7 @@ myEmitter.on('foundStores', (zip, stores, page) => {
 
 (async () => {
     console.log('zip codes:'+JSON.stringify(zipParam))
+    
     const browser = await puppeteer.launch({headless:false, executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'});
     const page = await browser.newPage();
     await page.goto('https://www.kroger.com/rx/guest/get-vaccinated');
